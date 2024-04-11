75e95624e32d5bb1428c438b879e9ea97495a89a
David Israel Granados Elias <israeldavi0904@gmail.com>
2024-04-11 15:59:15 -0500

    FIX: filter

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -20,6 +20,7 @@ export default function Filters() {
         type: ReducerCasesReport.MONTH,
         value: { year: report.year, month, data: null },
       });
+      setYear(undefined)
     }
     if (year) {
       dispatchReport({
@@ -95,6 +96,37 @@ export default function Filters() {
           Search
         </button>
       </form>
+      {!report.data?.listPerProjectType.length && (
+        <div
+          role='alert'
+          className='relative flex sm:w-5/6 m-auto mt-3 px-4 py-4 text-base text-white bg-orange-500 rounded-lg font-regular'
+          data-dismissible='alert'
+        >
+          <div className='mr-12 '>No hay datos para mostrar.</div>
+          <button
+            data-dismissible-target='alert'
+            className='!absolute  top-3 right-3 h-8 max-h-[32px] w-8 max-w-[32px] select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-white transition-all hover:bg-white/10 active:bg-white/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+            type='button'
+          >
+            <span className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                className='w-6 h-6'
+                strokeWidth='2'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M6 18L18 6M6 6l12 12'
+                ></path>
+              </svg>
+            </span>
+          </button>
+        </div>
+      )}
     </div>
   );
 }
